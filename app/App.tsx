305aa52956c67ad9fda1afc11fabfbac2e020744
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2022-10-04 00:28:10 -0300

    💄 add splash screen

--- app/App.tsx
+++ app/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 import Home from './screens/Home'
@@ -6,6 +7,7 @@ import Home from './screens/Home'
 const App = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar backgroundColor='#F8F8F8' barStyle='dark-content' />
       <Home />
     </GestureHandlerRootView>
   )
